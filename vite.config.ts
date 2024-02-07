83b918425019e5593a2303a14a8db8c9fcd46274
Jeroen Claessens <jeroen.claessens@jcss.be>
2024-02-07 13:49:56 +0100

    chore: improve type declarations by removing rollup-dts plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [vue(), dts({ rollupTypes: true })],
+  plugins: [vue()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
